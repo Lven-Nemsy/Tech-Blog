36da688cc83fc510fe941ccda787c59b80941ed1
Nedda Elsayed <nedda_e@yahoo.com>
2023-09-19 23:53:59 -0700

    update after tutoring

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { blog, User } = require('../models');
+const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all blogs and JOIN with user data
-    const blogData = await blog.findAll({
+    const blogData = await Blog.findAll({
       include: [
         {
           model: User,
@@ -50,29 +50,36 @@ router.get('/blog/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Blog }],
+     const blogData = await Blog.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
     });
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
+    res.render('dashboard', {
+      ...blogs,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+router.get('/dashboard/new', withAuth, (req, res)=> {
+  res.render('newBlog', {
+    logged_in: req.session.logged_in,
+  });
+})
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
