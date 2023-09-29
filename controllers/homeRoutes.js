5c906e84bef5e034b1338a37a341ca6cbe5f6525
Nedda Elsayed <nedda_e@yahoo.com>
2023-09-28 20:29:45 -0700

    Got Heroku working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,7 +50,7 @@ router.get('/blog/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req, res) => {
+router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
      const blogData = await Blog.findAll({
